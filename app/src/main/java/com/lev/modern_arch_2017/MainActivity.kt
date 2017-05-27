8deeeacbb31043e0c425f4aef0ffe6cff5553eeb
Lev Novikov <LevNovikov92@gmail.com>
2017-05-27 22:45:39 +0500

    Initial commit

---
+++ app/src/main/java/com/lev/modern_arch_2017/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lev.modern_arch_2017
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
